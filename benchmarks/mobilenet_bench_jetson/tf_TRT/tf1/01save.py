47c6f856c6cd2872c41c38df3baa42090a089212
Robert Krajnak <robertkrajnak1@gmail.com>
2022-04-29 14:35:13 +0200

    all work without models vol 3

--- benchmarks/mobilenet_bench_jetson/tf_TRT/tf1/01save.py
+++
@@ -1,9 +0,0 @@
-import os
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2 as Net
-
-model = Net(weights='imagenet')
-
-os.makedirs('./Desktop', exist_ok=True)
-
-# Save the h5 file to path specified.
-model.save("./Desktop/mobilenet.h5")
